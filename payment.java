66ebe07faa91160bf50209f4f52255e4614f425c
Sarvesh yadav <sarveshky221104@gmail.com>
2026-07-03 16:26:09 +0530

    Add Payment class with makePayment method

--- payment.java
+++ payment.java
@@ -1,1 +1,11 @@
 
+class Payment {
+
+    public static boolean makePayment(double amount) {
+
+        System.out.println("Payment of ₹" + amount + " Successful.");
+        return true;
+
+    }
+
+}
