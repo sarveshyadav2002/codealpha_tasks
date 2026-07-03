8ad44731cef1909c377b3486b3540a42aa6e6035
Sarvesh yadav <sarveshky221104@gmail.com>
2026-07-03 16:23:44 +0530

    Update hotal_booking.java

--- hotal_booking.java
+++ hotal_booking.java
@@ -1,1 +1,41 @@
+class Booking {
 
+    private String customerName;
+    private int roomNumber;
+    private String category;
+    private double amount;
+
+    public Booking(String customerName, int roomNumber,
+                   String category, double amount) {
+
+        this.customerName = customerName;
+        this.roomNumber = roomNumber;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+
+        return customerName + "," +
+                roomNumber + "," +
+                category + "," +
+                amount;
+    }
+}
