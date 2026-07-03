8f7e66ee279adb164c1171e130c89d2fab438846
Sarvesh yadav <sarveshky221104@gmail.com>
2026-07-03 16:30:18 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,86 @@
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        HotelSystem hotel = new HotelSystem();
+
+        while (true) {
+
+            System.out.println("\n===== HOTEL BOOKING SYSTEM =====");
+
+            System.out.println("1.Search Rooms");
+
+            System.out.println("2.Book Room");
+
+            System.out.println("3.Cancel Booking");
+
+            System.out.println("4.View Booking");
+
+            System.out.println("5.Exit");
+
+            System.out.print("Enter Choice : ");
+
+            int ch = sc.nextInt();
+
+            switch (ch) {
+
+                case 1:
+
+                    hotel.searchRooms();
+
+                    break;
+
+                case 2:
+
+                    System.out.print("Customer Name : ");
+
+                    sc.nextLine();
+
+                    String name = sc.nextLine();
+
+                    System.out.print("Room Number : ");
+
+                    int room = sc.nextInt();
+
+                    hotel.bookRoom(name, room);
+
+                    break;
+
+                case 3:
+
+                    System.out.print("Room Number : ");
+
+                    int r = sc.nextInt();
+
+                    hotel.cancelBooking(r);
+
+                    break;
+
+                case 4:
+
+                    hotel.viewBookings();
+
+                    break;
+
+                case 5:
+
+                    System.out.println("Thank You");
+
+                    System.exit(0);
+
+                default:
+
+                    System.out.println("Invalid Choice");
+
+            }
+
+        }
+
+    }
+
+}
